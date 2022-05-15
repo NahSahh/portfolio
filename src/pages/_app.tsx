46899ac29bf6caa91e514759dfbdc4d820adfe63
Jake <75503554+NahSahh@users.noreply.github.com>
2022-05-16 00:09:04 +0100

    feat: Loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
-export default function Portfolio({ Component, pageProps }: AppProps) {
+const Loader = () => {
+  return (
+    <div className="flex h-screen justify-center">
+      <div className="m-auto flex flex-col items-center">
+        <span className="mt-2 animate-pulse text-lg text-gray-300 font-semibold">
+          Loading
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export function Portfolio({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1200);
+  });
   return (
     <>
       <div className="min-h-screen dark:bg-slate-900 dark:text-white">
-        <Component {...pageProps} />
+        {loading ? <Loader /> : <Component {...pageProps} />}
       </div>
     </>
   );
 }
+
+export default Portfolio;
