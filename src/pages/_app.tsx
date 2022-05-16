2e880ae5ddc358afbf8cdf11ebd4f02b18ead7e9
Jake <75503554+NahSahh@users.noreply.github.com>
2022-05-16 20:24:16 +0100

    use next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,12 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 
 const Loader = () => {
   return (
     <>
-      <Head>
-        <title>{process.env.userName}</title>
-        <meta
-          name="title"
-          content={`${process.env.userName}'s Portfolio site`}
-        />
-        <meta
-          name="description"
-          content={`${process.env.userName}'s site, you should use Kythi.com btw.`}
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:image" content="nahsahh.png" />{" "}
-      </Head>
       <div className="flex h-screen justify-center">
         <div className="m-auto flex flex-col items-center">
           <span className="mt-2 animate-pulse text-lg text-gray-300 font-semibold">
@@ -39,6 +27,43 @@ export function Portfolio({ Component, pageProps }: AppProps) {
   });
   return (
     <>
+      <Head>
+        <title>{process.env.userName}</title>
+        <meta
+          name="title"
+          content={`${process.env.userName}'s Portfolio site`}
+        />
+        <meta
+          name="description"
+          content={`${process.env.userName}'s site, you should use Kythi.com btw.`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content="nahsahh.png" />{" "}
+      </Head>
+      <NextSeo
+        title={process.env.userName}
+        description={`${process.env.userName}'s site, you should use Kythi.com btw.`}
+        additionalMetaTags={[
+          {
+            property: "theme-color",
+            content: "#5E81AC",
+          },
+        ]}
+        openGraph={{
+          title: process.env.userName,
+          description:
+            process.env.userName + "'s site, you should use Kythi.com btw.",
+          images: [
+            {
+              url: "nahsahh.png",
+              width: 800,
+              height: 600,
+              alt:
+                process.env.userName + "'s site, you should use Kythi.com btw.",
+            },
+          ],
+        }}
+      />
       <div className={`min-h-screen dark:bg-gray-900 dark:text-white`}>
         {loading ? <Loader /> : <Component {...pageProps} />}{" "}
       </div>
