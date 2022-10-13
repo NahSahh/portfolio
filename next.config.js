12dae703cd62c9dacb98e48063166df448fa6b15
Jake <75503554+NahSahh@users.noreply.github.com>
2022-10-13 22:29:08 +0100

    fix: Tooltip.

--- next.config.js
+++ next.config.js
@@ -19,12 +19,10 @@ module.exports = {
         purpose: "Public File-sharing",
         paragraph:
           "Kythi is a public free-to-use file-uploader that allows you to upload images and share them with anyone, along with customized embeds to make them look like you want.",
-        Project1: "Kythi",
+        Project1: "Kythi Github",
         Plink1: "https://github.com/Kythix",
-        Project2: "",
-        Plink2: "",
-        Project3: "",
-        Plink3: "",
+        Project2: "The official discord server for kythi.",
+        Plink2: "https://discord.gg/kythi",
       },
       {
         title: "Portfolio",
@@ -33,9 +31,9 @@ module.exports = {
         purpose: "Portfolio Website",
         paragraph:
           "This portfolio, built with Next.js, is a website that showcases my projects and allows you to view the best ways to contact me.",
-        Project1: "Kythi",
-        Plink1: "https://github.com/",
-        Project2: "",
+        Project1: "Portfolio Github",
+        Plink1: "https://github.com/nahsahh/portfolio",
+        Project2: "Theres No Discord for this project.",
         Plink2: "",
       },
       {
@@ -45,9 +43,10 @@ module.exports = {
         purpose: "Private File Uploader",
         paragraph:
           "E-Z.host is a closed-private file-uploader, used to store uploads and customise embeds, it has a plethora of features to choose from allowing you to personalise your files, you can purchase it on their website.",          Project1: "Kythi",
-        Plink1: "https://github.com/",
-        Project2: "",
-        Plink2: "",
+        Project1: "There's no github for this project.",
+        Plink1: "",
+        Project2: "E-Z.host discord server",
+        Plink2: "https://discord.gg/ez",
       },
     ],
   },
