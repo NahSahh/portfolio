5b65c77aff795b6af64c22a0ecc750cb4f910021
Jake <75503554+NahSahh@users.noreply.github.com>
2022-10-13 22:00:06 +0100

    feat: Projects

--- src/interface/CardData.d.ts
+++ src/interface/CardData.d.ts
@@ -4,4 +4,10 @@ export interface Card {
   role: string;
   purpose: string;
   paragraph: string;
+  Project1: string;
+  Project2: string;
+  Project3: string;
+  Plink1: string;
+  Plink2: string;
+  Plink3: string;
 }
